37191bd0c2e853b409dde06bed4220764a2b2a44
Fariskdy <farisfarizz500@gmail.com>
2025-02-17 19:12:54 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,10 @@ function App() {
                   <Route path="login" element={<Login />} />
                   <Route path="register" element={<Register />} />
                   <Route path="forgot-password" element={<ForgotPassword />} />
-                  <Route path="reset-password/:token" element={<ResetPassword />} />
+                  <Route
+                    path="reset-password/:token"
+                    element={<ResetPassword />}
+                  />
                 </Routes>
               </AuthRoute>
             }
